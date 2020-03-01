623d400a4677dfc42de7be9421ee1c4fbb736376
kizzanaome <naomekizzan@gmail.com>
2020-03-01 16:39:37 +0300

    ft(auth): add validations.
    - Add discrpitive error messages to the username, email, pasword and phone number.

--- apps/authentication/api/serializers.py
+++ apps/authentication/api/serializers.py
@@ -1,44 +1,43 @@
 from django.contrib.auth import authenticate
 from django.core.exceptions import ValidationError
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
 
 from ..models import User
+from .utils import validate_username, validate_password, validate_email, validate_contact
+from django.db import IntegrityError
 
 
 class SignupSerializer(serializers.ModelSerializer):
 
-    password2 = serializers.CharField(
-        max_length=17,
-        min_length=7,
-        write_only=True
-    )
     email = serializers.EmailField()
     username = serializers.CharField()
     contact = serializers.CharField()
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'contact', 'password', 'password2']
+        fields = ['email', 'username', 'contact', 'password']
         extra_kwargs = {
             'password': {
                 'write_only': True
             },
+            
         }
 
+    def validate_username(self, username):
+        return validate_username(username)
+
+    def validate_password(self, password):
+        return validate_password(password)
+
+    def validate_email(self, email):
+        return validate_email(email)
+
+    def validate_contact(self, contact):
+        return validate_contact(contact)
+
     def create(self, validated_data):
-        user = User(email=self.validated_data['email'],
-                    username=self.validated_data['username'], contact=self.validated_data['contact'])
-        password = self.validated_data['password']
-        password2 = self.validated_data['password2']
-
-        if password != password2:
-            raise serializers.ValidationError(
-                {'password': 'Passwords must match'})
-
-        user.set_password(password)
-        user.save()
-
-        return user
+        return User.objects.create_user(**validated_data)
 
 
 class LoginSerializer(serializers.ModelSerializer):
@@ -56,11 +55,10 @@ class LoginSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         email = data.get('email', None)
-        password = data.get('password',None)
-        user = authenticate(username=email,password=password)
+        password = data.get('password', None)
+        user = authenticate(username=email, password=password)
         if user is None:
-                raise ValidationError("Incorrect credentials please try again")
+            raise ValidationError("Incorrect credentials please try again")
         return {
             'email': user.email
         }
-
